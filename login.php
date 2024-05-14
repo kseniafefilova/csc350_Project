1c7e79f8aab3c9357fdfc435834d2762af82601d
Florian Charles <floriand.charles@gmail.com>
2024-05-14 18:48:27 -0400

    Added the database for the differents admins

--- login.php
+++ login.php
@@ -5,8 +5,11 @@ $password = "";
 $dbname = "CIS385";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
