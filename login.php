6abd26f72c23030997b11eae4567f1ad61a7ca36
Florian Charles <floriand.charles@gmail.com>
2024-05-17 16:42:27 -0400

    A version of the register.html and php

--- login.php
+++ login.php
@@ -2,9 +2,7 @@
 $servername = "localhost";
 $username = "floflo";
 $password = "";
-$dbname = "CIS385";
-
-
+$dbname = "CSC350";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -16,8 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $pass = $_POST["pwd"];
 
-
-    if ($stmt = $conn->prepare("SELECT password FROM faculty WHERE email = ?")) {
+    if ($stmt = $conn->prepare("SELECT password FROM Admin WHERE email = ?")) {
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $stmt->store_result();
@@ -26,9 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stmt->bind_result($password_from_db);
             $stmt->fetch();
 
-
             if ($pass == $password_from_db) {
-
                 $_SESSION["loggedin"] = true;
                 $_SESSION["email"] = $email;
 
@@ -36,12 +31,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location: index.html");
                 exit();
             } else {
-                echo "Invalid email or password.";
-                header("Location: index.html");
+                header("Location: login.html");
             }
         } else {
             echo "Invalid email or password.";
-            header("Location: index.html");
         }
 
         $stmt->close();
@@ -54,4 +47,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <br>
 <a href="login.html">Press this to go back</a>
-
